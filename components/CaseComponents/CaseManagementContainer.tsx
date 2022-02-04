7dd3b6b3e19de83230951ad0b9e0a5984361574f
kellyjding <kellyding@college.harvard.edu>
2022-02-04 16:05:39 -0500

    change

--- components/CaseComponents/CaseManagementContainer.tsx
+++ components/CaseComponents/CaseManagementContainer.tsx
@@ -59,7 +59,7 @@ const CaseManagementContainer: React.FC = (props) => {
         
         {data
             ? data.cases.map((index : number, value : any) => {
-                return <CaseCategory category_id={index} data={value}/>;
+                return <CaseCard key={index} data={value}/>;
               })
             : "Something went wrong"}
 
