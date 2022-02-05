baf6f4909d050e3c9e1eed18795aae4c5e08bf41
kellyjding <kellyding@college.harvard.edu>
2022-02-05 18:02:39 -0500

    fin

--- components/CaseComponents/CaseManagementContainer.tsx
+++ components/CaseComponents/CaseManagementContainer.tsx
@@ -59,7 +59,7 @@ const CaseManagementContainer: React.FC = (props) => {
         
         {/* format in grid  */}
         {data
-            ? data.category.map((index: number, value : ManagementCategory) => {
+            ? data.category.map((index: number, value : any) => {
                 return <Grid item xs={3} key={index}>
                     <CaseCategory category_id={value} />
                 </Grid> 
